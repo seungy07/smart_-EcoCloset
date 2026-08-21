a7959d6d3d6823e2e5b495f316ab81b4d842afe0
yoohyun0830-ship-it <yoohyun0830@gmail.com>
2026-08-21 18:18:29 +0900

    MemberController 회원가입까지 완성

--- src/controller/MemberController.java
+++ src/controller/MemberController.java
@@ -1,7 +1,41 @@
 package controller;
 
+import model.dao.MemberDao;
+import model.dto.MemberDto;
+
 public class MemberController {
-    public static void main(String[] args) {
-        
-    }
+    // 싱글톤
+   private MemberController(){}
+
+   private static final MemberController instance =new MemberController();
+
+   public static MemberController getInstance(){
+        return instance;
+   }
+
+   // DAO 객체
+   private MemberDao memberDao = MemberDao.getInstance();
+
+   // 현재 로그인한 회원
+   private MemberDto loginMember = null;
+
+   // [1] 아이디 유효성 검사
+   public boolean inCheck(String m_id){
+    return memberDao.inCheck(m_id);
+   }
+
+   // [2] 회원가입
+   public boolean signUp(String m_id, String m_pwd){
+    // 아이디 중복검사
+    boolean check = memberDao.inCheck(m_id);
+
+    // 이미 아이디가 존재할 때
+    if(check == false){ return false;}
+
+    MemberDto memberDto = new MemberDto(m_id, m_pwd);
+    return memberDao.signUp(memberDto);
+   }
+
+   // [3] 로그인
+   
 }
--- src/view/MemberView.java
+++ src/view/MemberView.java
@@ -32,9 +32,7 @@ public class MemberView {
             else if( ch == 2){
                 signUp();
             }
-            else if( ch == 3){
-                System.out.println("프로그램을 종료합니다.");
-            } 
+            else if( ch == 3){System.out.println("프로그램을 종료합니다.");} 
             else{{System.out.println("[경고] 잘못된 번호입니다.");}}
         }
 
