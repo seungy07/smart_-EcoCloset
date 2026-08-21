7a63f642d272879d134f0a7b3aa6e2083cc5ab13
limjunhee <ski725400@gmail.com>
2026-08-21 18:02:26 +0900

    순환처리MVC파일생성-임준희

---
+++ src/controller/RecycleController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.util.ArrayList;
+
+import model.dao.RecycleDao;
+import model.dto.RecycleDto;;
+
+public class RecycleController {
+    private RecycleController(){};
+    private static final RecycleController instance = new RecycleController();
+    public static RecycleController getInstance() { return instance; }
+    private RecycleDao rd = RecycleDao.getInstance();
+
+
+    public ArrayList<RecycleDto> unusedReport(){
+        ArrayList<RecycleDto> result = rd.unusedReport();
+
+        return result;
+    }
+}
