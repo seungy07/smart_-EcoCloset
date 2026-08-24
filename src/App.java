8750102dcd0569b9140398f5ca5aca3d4b973276
yoohyun0830-ship-it <yoohyun0830@gmail.com>
2026-08-24 15:41:10 +0900

    memberController

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import view.MemberView;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        MemberView.getInstance().run();
     }
 }
--- src/controller/MemberController.java
+++ src/controller/MemberController.java
@@ -37,5 +37,54 @@ public class MemberController {
    }
 
    // [3] 로그인
-   
+   public boolean login(String m_id, String m_pwd){
+     MemberDto memberDto = memberDao.login(m_id, m_pwd);
+     if(memberDto == null){
+          return false;
+     }
+     loginMember = memberDto;
+     return true;
+   }
+
+   // [4] 로그아웃
+   public void logout(){
+     loginMember = null;
+   }
+
+   // [5] 현재 로그인 회원 반환
+   public MemberDto getLoginMember(){
+     return loginMember;
+   }
+
+   // [6] 회원탈퇴
+   public boolean memberDelete(){
+     if(loginMember == null){
+          return false;
+     }
+     int m_no = loginMember.getM_no();
+     boolean result = memberDao.memberDelete(m_no);
+
+     if(result == true){
+          loginMember = null;
+     }
+     return result;
+   }
+
+   // [7] Eco Level 계산
+   public String ecoLevelCheck(int ecoPoint){
+     if(ecoPoint < 100){
+          return "LV.1 씨앗";}
+          else if(ecoPoint < 250){
+            return "Lv.2 새싹";
+        }
+        else if(ecoPoint < 500){
+            return "Lv.3 나무";
+        }
+        else if(ecoPoint < 800){
+            return "Lv.4 큰 나무";
+        }
+        else{
+            return "Lv.5 숲";
+   }
+}
 }
